00bf47a74624917f94939361bbcd43be88218359
Markus <markus@greenrobot>
2024-05-14 17:52:03 +0200

    local-rag.py: minor improvements #1

--- examples/local-rag/local-rag.py
+++ examples/local-rag/local-rag.py
@@ -8,7 +8,7 @@ embeddings = OllamaEmbeddings(model="znbang/bge:small-en-v1.5-f32")  # 134 MB, 3
 embeddings_dim = 384
 
 print("Let's go...")
-embedding_vector = embeddings.embed_query("How to create a ObjectBox query in Kotlin?")
+embedding_vector = embeddings.embed_query("How to create a ObjectBox query in Java?")
 print("Embedding query vector:", len(embedding_vector))
 print(embedding_vector[:10])
 
@@ -33,11 +33,14 @@ text_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
 documents = text_splitter.split_documents(docs)
 print("Documents splitted: ", len(documents))
 
-objectbox = ObjectBox.from_documents(documents, embeddings, embedding_dimensions=embeddings_dim, do_log=True, clear_db=True)
-print("ObjectBox ready")
+objectbox = ObjectBox.from_documents(documents, embeddings, embedding_dimensions=embeddings_dim, do_log=True,
+                                     clear_db=True)
 
+# These 2 lines are not part of the document chain but show how to use ObjectBox more directly (and print the results)
 results = objectbox.similarity_search_by_vector(embedding_vector, 2)
 print(results)
+
+print("Generating result...")
 
 document_chain = create_stuff_documents_chain(llm, prompt)
 from langchain.chains import create_retrieval_chain
@@ -46,5 +49,10 @@ retriever = objectbox.as_retriever()
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
 response = retrieval_chain.invoke(
-    {"input": "How to create a ObjectBox query in Kotlin, e.g. for an existing entity Person? Search by address Sesamestreet 1"})
+    {
+        "input": "How to create a ObjectBox query in Java, e.g. for an existing entity Person? "
+                 "Search by address Sesame Street 1"})
 print(response["answer"])
+
+# Note: the quality of the answer is not great, but given that we feed it only with a few lines of input, it's not bad.
+#       The more information you can provide, the better the answer will be.
